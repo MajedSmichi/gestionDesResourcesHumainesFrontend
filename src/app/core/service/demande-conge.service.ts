8e3977a4a1b4754a4e2eb74279e2d1ccc1ad8561
Majed Smichi <smichimajed@gmail.com>
2024-08-08 12:37:05 +0100

    Accept or refused Demand Conge

--- src/app/core/service/demande-conge.service.ts
+++ src/app/core/service/demande-conge.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import {catchError, Observable, throwError} from 'rxjs';
 import { DemandeConge } from '../model/demande-conge.model';
+import { DemandeCongeStatus } from '../model/demande-conge-status.enum';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +12,41 @@ export class DemandeCongeService {
 
   constructor(private http: HttpClient) {}
 
-
-
   saveDemandeConge(demande: DemandeConge): Observable<DemandeConge> {
     return this.http.post<DemandeConge>(`${this.apiUrl}/save`, demande);
   }
 
-
   getDemandesConge(): Observable<DemandeConge[]> {
     return this.http.get<DemandeConge[]>(`${this.apiUrl}/all`);
   }
+
+  refuseDemandeConge(id: number | undefined, status: DemandeCongeStatus): Observable<DemandeConge> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    // Créez un objet avec le statut
+    const body = { status };
+
+    return this.http.put<DemandeConge>(`${this.apiUrl}/refuseDemandeConge/${id}`, body, { headers }).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.error('Error updating demande conge status:', error);
+        return throwError(() => new Error('Erreur lors de la mise à jour du statut de la demande de congé'));
+      })
+    );
+  }
+
+
+  acceptDemandeConge(id: number | undefined, status: DemandeCongeStatus): Observable<DemandeConge> {
+    const body = { status };
+    return this.http.put<DemandeConge>(`${this.apiUrl}/acceptDemandeConge/${id}`, body).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.error('Error updating demande conge status:', error);
+        return throwError(() => new Error('Erreur lors de la mise à jour du statut de la demande de congé'));
+      })
+    );
+  }
+
+
+
+
 
 }
--- src/app/pages/Conge/list-conge/list-conge.component.ts
+++ src/app/pages/Conge/list-conge/list-conge.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {DemandeConge} from "../../../core/model/demande-conge.model";
-import {DemandeCongeService} from "../../../core/service/demande-conge.service";
-
+import { DemandeConge } from "../../../core/model/demande-conge.model";
+import { DemandeCongeService } from "../../../core/service/demande-conge.service";
+import { DemandeCongeStatus } from '../../../core/model/demande-conge-status.enum';
 
 @Component({
   selector: 'app-list-conge',
@@ -16,22 +16,45 @@ export class ListCongeComponent implements OnInit {
 
   constructor(private demandeCongeService: DemandeCongeService) {}
 
-
   ngOnInit(): void {
     this.loadDemandes();
+  }
+
+  loadDemandes(): void {
     this.demandeCongeService.getDemandesConge().subscribe(data => {
       this.demandes = data.map(demande => {
         demande.user.photo = `http://localhost:9090/rh/uploads/${demande.user.photo}`;
         return demande;
       });
-    });
-  }
-
-  loadDemandes(): void {
-    this.demandeCongeService.getDemandesConge().subscribe(data => {
-      this.demandes = data;
     }, error => {
       console.error('Error fetching demandes conge:', error);
     });
+  }
+
+  acceptDemande(demande: DemandeConge): void {
+    this.demandeCongeService.acceptDemandeConge(demande.id, DemandeCongeStatus.APPROVED).subscribe(() => {
+      console.log('Accepting demande:', demande);
+      this.loadDemandes();
+    });
+  }
+
+  refuseDemande(demande: DemandeConge): void {
+    this.demandeCongeService.refuseDemandeConge(demande.id, DemandeCongeStatus.REJECTED).subscribe(() => {
+      console.log('Refusing demande:', demande);
+      this.loadDemandes();
+    });
+  }
+
+  getStatusClass(status: string): string {
+    switch (status) {
+      case 'APPROVED':
+        return 'text-success';
+      case 'REJECTED':
+        return 'text-danger';
+      case 'PENDING':
+        return 'text-primary';
+      default:
+        return '';
+    }
   }
 }
