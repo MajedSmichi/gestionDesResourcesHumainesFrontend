bd3889fa90c536e9b77b335b754d9ca79882137b
MajedSmichi <smichimajed@gmail.com>
2024-08-15 00:57:38 +0100

    fixed search and filter with post

--- src/app/core/model/poste.model.ts
+++ src/app/core/model/poste.model.ts
@@ -2,3 +2,4 @@ export class Poste {
   poste_id!: number;
   posteType!: string;
 }
+
--- src/app/pages/user/user-list/user-list.component.ts
+++ src/app/pages/user/user-list/user-list.component.ts
@@ -1,4 +1,3 @@
-// user-list.component.ts
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../../core/model/user.model';
 import { UserService } from '../../../core/service/user.service';
@@ -6,18 +5,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../core/service/auth.service';
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-employe-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
   standalone: true,
-  imports: [HttpClientModule, CommonModule],
+  imports: [HttpClientModule, CommonModule, FormsModule],
   providers: [UserService]
 })
 export class UserListComponent implements OnInit {
   users: User[] = [];
+  filteredUsers: User[] = [];
   authenticatedUserId: number | null = null;
+  postes: string[] = [];
+  selectedPoste: string = '';
+  noEmployeesMessage: string = '';
 
   constructor(private userService: UserService, private router: Router, private authService: AuthService) { }
 
@@ -27,12 +31,49 @@ export class UserListComponent implements OnInit {
         user.photo = `http://localhost:9090/rh/uploads/${user.photo}`;
         return user;
       });
+      this.filteredUsers = this.users;
+    });
+    this.fetchPostes();
+  }
+
+  fetchPostes(): void {
+    this.userService.getPostes().subscribe(data => {
+      this.postes = data;
     });
   }
 
+  searchUsers(searchTerm: string): void {
+    if (searchTerm) {
+      this.filteredUsers = this.users.filter(user => user.email.toLowerCase().includes(searchTerm.toLowerCase()));
+    } else {
+      this.filteredUsers = this.users;
+    }
+    // Reset the message when searching
+    this.noEmployeesMessage = this.filteredUsers.length === 0 ? 'Aucun employé ne correspond à ce critère de recherche.' : '';
+  }
+
+  filterByPoste(selectedPoste: string): void {
+    if (selectedPoste) {
+      this.filteredUsers = this.users.filter(user =>
+        user.postes.some(poste => poste.posteType === selectedPoste)
+      );
+      this.noEmployeesMessage = this.filteredUsers.length === 0 ? 'Aucun employé ne correspond à ce poste.' : '';
+    } else {
+      this.filteredUsers = this.users;
+      this.noEmployeesMessage = '';
+    }
+  }
+
+  onPosteChange(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    this.selectedPoste = target.value;
+    this.filterByPoste(this.selectedPoste);
+  }
+
   deleteUser(id: number) {
-    this.userService.deleteUser(id).subscribe(data => {
+    this.userService.deleteUser(id).subscribe(() => {
       this.users = this.users.filter(e => e.id !== id);
+      this.filteredUsers = this.filteredUsers.filter(e => e.id !== id);
       this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
         this.router.navigate(['/user-list']);
       });
@@ -42,6 +83,12 @@ export class UserListComponent implements OnInit {
   enableUser(id: number) {
     this.userService.enableUser(id).subscribe(() => {
       this.users = this.users.map(user => {
+        if (user.id === id) {
+          user.active = true;
+        }
+        return user;
+      });
+      this.filteredUsers = this.filteredUsers.map(user => {
         if (user.id === id) {
           user.active = true;
         }
@@ -58,13 +105,18 @@ export class UserListComponent implements OnInit {
         }
         return user;
       });
+      this.filteredUsers = this.filteredUsers.map(user => {
+        if (user.id === id) {
+          user.active = false;
+        }
+        return user;
+      });
     });
   }
 
   viewUser(user: User) {
     this.router.navigate(['/user-detail', user.id]);
   }
-
 
   editUser(user: User) {
     this.router.navigate(['/user-detail', user.id], { queryParams: { edit: true } });
