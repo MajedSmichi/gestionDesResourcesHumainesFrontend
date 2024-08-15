081cae4c2f2fe082918764dcf06f2ecd3f9a62ad
MajedSmichi <smichimajed@gmail.com>
2024-08-15 20:34:01 +0100

    fix pieChartElement

--- src/app/layouts/home/home.component.ts
+++ src/app/layouts/home/home.component.ts
@@ -5,8 +5,11 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import bootstrap5Plugin from '@fullcalendar/bootstrap5';
 import { DemandeCongeService } from '../../core/service/demande-conge.service';
+import { UserService } from '../../core/service/user.service';
 import { DemandeConge } from '../../core/model/demande-conge.model';
-import {NgIf, NgStyle} from "@angular/common";
+import { User } from '../../core/model/user.model';
+import { NgIf, NgStyle } from '@angular/common';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-home',
@@ -19,9 +22,17 @@ export class HomeComponent implements OnInit {
 
   hoveredEvent: any = null; // Propriété pour stocker l'événement survolé
 
-  constructor(private demandeCongeService: DemandeCongeService) {}
+  constructor(
+    private demandeCongeService: DemandeCongeService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
+    this.initializeCalendar();
+    this.initializePieChart();
+  }
+
+  private initializeCalendar(): void {
     var calendarEl = document.getElementById('calendar');
 
     if (calendarEl) {
@@ -70,5 +81,48 @@ export class HomeComponent implements OnInit {
       });
 
     }
+  }
+
+  private initializePieChart(): void {
+    this.userService.getAllUser().subscribe((users: User[]) => {
+      const posteCounts = users.reduce((acc: { [key: string]: number }, user) => {
+        // Handle first poste or customize to aggregate multiple postes
+        if (user.postes.length > 0) {
+          const posteType = user.postes[0].posteType;
+          acc[posteType] = (acc[posteType] || 0) + 1;
+        }
+        return acc;
+      }, {});
+
+      const labels = Object.keys(posteCounts);
+      const data = Object.values(posteCounts);
+
+      const pieChartElement = document.getElementById("chartjs-pie");
+      if (pieChartElement) {
+        new Chart(pieChartElement as HTMLCanvasElement, {
+          type: "pie",
+          data: {
+            labels: labels,
+            datasets: [{
+              data: data,
+              backgroundColor: [
+                "#007bff", // primary
+                "#ffc107", // warning
+                "#dc3545", // danger
+                "#28a745", // success
+                "#17a2b8", // info
+                "#6c757d", // secondary
+                "#343a40", // dark
+                "#f8f9fa"  // light
+              ],
+              borderColor: "transparent"
+            }]
+          },
+          options: {
+            maintainAspectRatio: false
+          }
+        });
+      }
+    });
   }
 }
